018f13a7124f16992582b4e76ebd3766f5078987
ankush <ankushsharma0199@gmail.com>
2022-07-01 13:54:52 +0530

    init

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'android.debug2.app2',
+  appName: 'SecondIonic',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
---
+++ src/app/api.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import{HttpClient} from'@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(public http:HttpClient) { }
+  getdata(topic){
+   return this.http.get('https://newsapi.org/v2/everything?q='+topic+'&from=2022-05-27&sortBy=publishedAt&apiKey=a8c4cdf6369840d7994eac5ee431e1d0')
+  }
+}
